154e3d34b4cc0567e83ead197eec0c27f2a67daf
Paul Signorelli <135723496+psignorelli2@users.noreply.github.com>
2025-02-27 14:54:51 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `AI Car Assistant`;
-export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `Automobile History AI!`;
-export const CLEAR_BUTTON_TEXT: string = `CLEAR BUTTON TEXT`;
-export const PAGE_TITLE: string = `CarAI`;
+export const CHAT_HEADER: string = `NCAA Assistant`;
+export const MESSAGE_PLACEHOLDER: string = `Enter questions about NCAA rules and guidelines`;
+export const FOOTER_MESSAGE: string = `NCAA Division 1 Athletics`;
+export const CLEAR_BUTTON_TEXT: string = `CLEAR CHAT`;
+export const PAGE_TITLE: string = `Athletics AI`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
